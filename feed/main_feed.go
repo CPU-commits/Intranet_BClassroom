c2908766ae10c15a16305513927c8af5a4497de6
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-27 17:15:24 -0400

    [add] Main server

---
+++ feed/main_feed.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/CPU-commits/Intranet_BClassroom/feed/server"
+)
+
+func main() {
+	server.Init()
+}
---
+++ query/main_query.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/CPU-commits/Intranet_BClassroom/query/server"
+
+func main() {
+	server.Init()
+}
