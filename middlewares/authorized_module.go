cc6b8aafc63b7a2042899b7042a60b66eba79323
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-27 17:12:47 -0400

    [add] Middlewares AuthorizedModule and Cors

---
+++ middlewares/authorized_module.go
@@ -0,0 +1,52 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/CPU-commits/Intranet_BClassroom/models"
+	"github.com/CPU-commits/Intranet_BClassroom/res"
+	"github.com/CPU-commits/Intranet_BClassroom/services"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var workModel = models.NewWorkModel()
+
+func AuthorizedRouteModule() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		idModule := ctx.Param("idModule")
+		if idModule == "" && ctx.Param("idWork") != "" {
+			idObjWork, err := primitive.ObjectIDFromHex(ctx.Param("idWork"))
+			if err != nil {
+				ctx.AbortWithStatusJSON(http.StatusBadRequest, &res.Response{
+					Success: false,
+					Message: err.Error(),
+				})
+				return
+			}
+
+			var work *models.Work
+			cursor := workModel.GetByID(idObjWork)
+			if err := cursor.Decode(&work); err != nil {
+				ctx.AbortWithStatusJSON(http.StatusNotFound, &res.Response{
+					Success: false,
+					Message: fmt.Sprintf("No existe el trabajo indicado"),
+				})
+				return
+			}
+			idModule = work.Module.Hex()
+		}
+
+		claims, _ := services.NewClaimsFromContext(ctx)
+		authorized := services.AuthorizedRouteFromIdModule(idModule, claims)
+		if authorized != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, &res.Response{
+				Success: false,
+				Message: authorized.Error(),
+			})
+			return
+		}
+		ctx.Next()
+	}
+}
