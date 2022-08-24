cb8c44c27e2a2e5aa32f01ae6584aaf5f19f82de
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-08-24 06:10:26 -0400

    [add] Notification type

--- res/response.go
+++ res/response.go
@@ -1,7 +1,22 @@
 package res
 
+const (
+	PUBLICATION = "publication"
+	WORK        = "work"
+	GRADE       = "grade"
+)
+
 type Response struct {
 	Success bool                   `json:"success"`
 	Message string                 `json:"message,omitempty"`
 	Data    map[string]interface{} `json:"body,omitempty"`
+}
+
+type NotifyClassroom struct {
+	Title  string
+	Link   string
+	Where  string
+	Room   string
+	Type   string
+	IDUser string
 }
